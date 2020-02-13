2f2c71ee04589bf5b8311d58ea048ac6c0e7410d
eajadull <jadulled@gmail.com>
2020-02-13 19:47:32 -0300

    Agregar archivos de proyecto.

---
+++ UI.Cart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UI.Cart.Startup))]
+namespace UI.Cart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
